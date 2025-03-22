77cb739399b7093ac1bf0be685a984868f07f7d6
francielle oliveira da luz <francielleoliveiradaluz@Airdefrancielle.Princess.local>
2025-03-22 13:45:54 -0300

    chains of responsability

---
+++ src/main/java/br/com/alura/adopet/api/validacoes/TutorComAdocaoEmAndamento.java
@@ -0,0 +1,31 @@
+package br.com.alura.adopet.api.validacoes;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDTO;
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.Adocao;
+import br.com.alura.adopet.api.model.StatusAdocao;
+import br.com.alura.adopet.api.model.Tutor;
+import br.com.alura.adopet.api.repository.AdocaoRepository;
+import br.com.alura.adopet.api.repository.PetRepository;
+import br.com.alura.adopet.api.repository.TutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class TutorComAdocaoEmAndamento {
+    @Autowired
+    private AdocaoRepository repository;
+
+    @Autowired
+    private TutorRepository tutorRepository;
+
+    public void validacao(SolicitacaoAdocaoDTO dto) {
+        List<Adocao> adocoes = repository.findAll();
+        Tutor tutor = tutorRepository.getReferenceById(dto.idTutor());
+        for (Adocao a : adocoes) {
+            if (a.getTutor() == tutor && a.getStatus() == StatusAdocao.AGUARDANDO_AVALIACAO) {
+                throw new ValidacaoException("Tutor já possui outra adoção aguardando avaliação!");
+            }
+        }
+    }
+}
